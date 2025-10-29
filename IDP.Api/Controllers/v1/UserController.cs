77583e8547ff2d6b07c2c95f6081a7edb826f718
shabn <shabnajva@gmail.com>
2025-10-29 09:31:26 +0330

    Add project files.

---
+++ IDP.Api/Controllers/v1/UserController.cs
@@ -0,0 +1,29 @@
+﻿using IDP.Api.Controllers.BaseController;
+using IDP.application.Commands.user;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IDP.Api.Controllers.v1
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class UserController : IBaseController
+    {
+        public readonly IMediator _mediator;
+        public UserController(IMediator mediator)
+        {
+                _mediator = mediator;
+        }
+        /// <summary>
+        /// ورود اطلاعات کاربر 
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("Insert")]
+        public async Task<ActionResult> Insert([FromBody ] UserCommand userCommand)
+        {
+            _mediator.Send(userCommand);
+            return Ok();
+        } 
+    }
+}
---
+++ IDP.application/Commands/user/UserCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IDP.application.Commands.user
+{
+    public class UserCommand:IRequest<bool>
+    {
+        [Required(ErrorMessage ="نام الزامی است ")]
+        [MinLength(4)]
+        public string Name { get; set; }
+    }
+}
